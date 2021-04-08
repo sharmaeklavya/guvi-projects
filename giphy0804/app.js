0358b7d2945d1d38a40413f8e7ebcab5c8bda645
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-04-09 01:06:51 +0530

    Update app.js

--- giphy0804/app.js
+++ giphy0804/app.js
@@ -41,17 +41,17 @@ const apiKey = "&api_key=YOrFrobTbZln2vz9RNNpA66vwGVuZGsP&limit=5";
 const button = document.querySelector(".button");
 
 //Event listener
-fetch(randomUrl)
-  .then((res) => res.json())
-  .then((word) => {
-    return fetch(giphyUrl + word.toString() + apiKey);
-  })
-  .then((res) => res.json())
-  .then((url) => {
-    button.addEventListener("click", () => {
+button.addEventListener("click", () => {
+  fetch(randomUrl)
+    .then((res) => res.json())
+    .then((word) => {
+      return fetch(giphyUrl + word.toString() + apiKey);
+    })
+    .then((res) => res.json())
+    .then((url) => {
       giphyGen(url);
     });
-  });
+});
 
 //Function to generate gifhy
 function giphyGen(url) {
