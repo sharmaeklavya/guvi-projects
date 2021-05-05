91e17ca609012a0a91fd16eb86ef00f3ff235d9e
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-05-05 20:56:00 +0530

    update

---
+++ dictionary2304/iwords.js
@@ -0,0 +1,220 @@
+const iWords = [
+  {
+    name: "zenith",
+    def: "the highest point of something",
+  },
+  {
+    name: "zealot",
+    def: "a fervent and even militant proponent of something",
+  },
+  {
+    name: "yearn",
+    def: "desire strongly or persistently",
+  },
+  {
+    name: "yawner",
+    def: "a person who yawns",
+  },
+  {
+    name: "xenophobia",
+    def: "a fear of foreigners or strangers",
+  },
+  {
+    name: "x-axis",
+    def: "the horizontal axis in a plane coordinate system",
+  },
+  {
+    name: "wonky",
+    def: "turned or twisted toward one side",
+  },
+  {
+    name: "wanton",
+    def: "a lewd or immoral person",
+  },
+  {
+    name: "vermillion",
+    def: "of a vivid red to reddish-orange color",
+  },
+  {
+    name: "vague",
+    def: "lacking clarity or distinctness",
+  },
+
+  {
+    name: "unique",
+    def: "the single one of its kind",
+  },
+  {
+    name: "uncanny",
+    def: "surpassing the ordinary or normal",
+  },
+  {
+    name: "tenacious",
+    def: "stubbornly unyielding",
+  },
+  {
+    name: "tangible",
+    def: "perceptible by the senses, especially the sense of touch",
+  },
+  {
+    name: "serene",
+    def: "not agitated",
+  },
+
+  {
+    name: "saquinavir",
+    def: "a weak protease inhibitor used in treating HIV",
+  },
+  {
+    name: "rhetorical",
+    def: "relating to using language effectively",
+  },
+  {
+    name: "rambunctious",
+    def: "noisy and lacking in restraint or discipline",
+  },
+  {
+    name: "quixotic",
+    def: "not sensible about practical matters",
+  },
+  {
+    name: "quell",
+    def: "suppress or crush completely",
+  },
+  {
+    name: "pique",
+    def: "call forth, as an emotion, feeling, or response",
+  },
+  {
+    name: "paradigm",
+    def: "a standard or typical example",
+  },
+  {
+    name: "oxymoron",
+    def: "conjoined contradictory terms",
+  },
+
+  {
+    name: "optimistically",
+    def: "with optimism; in an optimistic manner ",
+  },
+  {
+    name: "nostalgic",
+    def: "unhappy about being away and longing for familiar things",
+  },
+  {
+    name: "narrative",
+    def: "an account that tells the particulars of an act or event",
+  },
+  {
+    name: "misanthrope",
+    def: "someone who dislikes people in general",
+  },
+  {
+    name: "melancholy",
+    def: "a constitutional tendency to be gloomy and depressed",
+  },
+  {
+    name: "lucid",
+    def: "transparently clear; easily understandable",
+  },
+  {
+    name: "lethargic",
+    def: "deficient in alertness or activity",
+  },
+  {
+    name: "ken",
+    def: "range of what one can know or understand",
+  },
+  {
+    name: "karma",
+    def: "effects of one's actions that determine his or her destiny",
+  },
+  {
+    name: "jurisdiction",
+    def: "the territory within which power can be exercised",
+  },
+
+  {
+    name: "jejune",
+    def: "lacking interest or significance or impact",
+  },
+  {
+    name: "irony",
+    def: "incongruity between what might be expected and what occurs",
+  },
+
+  {
+    name: "integrity",
+    def: "an undivided or unbroken completeness with nothing wanting",
+  },
+
+  {
+    name: "hypnosis",
+    def: "a state that resembles sleep induced by suggestion",
+  },
+  {
+    name: "hyperbole",
+    def: "extravagant exaggeration",
+  },
+  {
+    name: "guise",
+    def: "an artful or simulated semblance",
+  },
+  {
+    name: "gallivant",
+    def: "wander aimlessly in search of pleasure",
+  },
+
+  {
+    name: "fortitude",
+    def: "strength of mind that enables one to endure adversity",
+  },
+  {
+    name: "fervent",
+    def: "characterized by intense emotion",
+  },
+
+  {
+    name: "esoteric",
+    def: "understandable only by an enlightened inner circle",
+  },
+  {
+    name: "empathy",
+    def: "understanding and entering into another's feelings",
+  },
+  {
+    name: "dubious",
+    def: "fraught with uncertainty or doubt",
+  },
+  {
+    name: "disposition",
+    def: "your usual mood",
+  },
+  {
+    name: "cynical",
+    def: "believing the worst of human nature and motives",
+  },
+
+  {
+    name: "capricious",
+    def: "determined by chance or impulse rather than by necessity",
+  },
+  {
+    name: "bypass",
+    def: "avoid something",
+  },
+  {
+    name: "benevolent",
+    def: "showing or motivated by sympathy and understanding",
+  },
+
+  {
+    name: "ambiguous",
+    def: "having more than one possible meaning",
+  },
+  {
+    name: "alliteration",
+    def: "use of the same consonant at the beginning of each word",
+  },
+];
--- dictionary2304/script.js
+++ dictionary2304/script.js
@@ -1,39 +1,111 @@
-//selector
+//selectors
 const form = document.querySelector("form");
 const cardRow = document.querySelector(".card__row");
+const listGroup = document.querySelector(".list__group");
+const monthWord = document.querySelector(".month-word");
 
-//Dictionary API
-const token = config.apiURI;
-const key = config.apiKEY;
-
+//Dictionary API call
 async function apiCall(word) {
-  const promise = await fetch(token + word + key);
+  const promise = await fetch(apiURI + word + apiKEY);
   const json = await promise.json();
-  console.log(json);
-  cardRow.innerHTML = "";
-
-  for (let i = 0; i < json.length; i++) {
-    const cardCol = bootstrap("div", "col-lg-6");
-    const card = bootstrap("div", "card mt-4");
-    const cardBody = bootstrap("div", "card-body");
-    cardBody.style.height = "15rem";
-    cardBody.innerHTML = `<h5 class="card-title">${json[i].hwi.hw}</h5>
-      <p class="card-text">${json[i].fl}</p>
-      <p class="card-text">${json[i].shortdef}</p>`;
-    card.appendChild(cardBody);
-    cardCol.appendChild(card);
-    cardRow.appendChild(cardCol);
-  }
+  searchWords(json);
 }
-
+// Event Listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const value = document.querySelector(".form__control").value;
   apiCall(value);
 });
 
+monthWord.addEventListener("click", () => {
+  apiCall(iWords[randomNum()].name);
+});
+
+// Resuable Functions
 function bootstrap(ele, className = "") {
   const element = document.createElement(ele);
   element.setAttribute("class", className);
   return element;
 }
+
+function randomNum() {
+  const random = Math.floor(Math.random() * iWords.length);
+  return random;
+}
+
+function card(col, body) {
+  const cardCol = bootstrap("div", "col-lg-" + col);
+  const card = bootstrap("div", "card mt-4");
+  const cardBody = bootstrap("div", "card-body overflow-auto");
+  cardBody.style.height = "15rem";
+  cardBody.innerHTML = body;
+  card.appendChild(cardBody);
+  cardCol.appendChild(card);
+  cardRow.appendChild(cardCol);
+}
+
+// function to search words
+function searchWords(words) {
+  cardRow.innerHTML = "";
+  if (words.length === 0) {
+    const notFound = `<h5 class="card-title">No Result Found... try searching with different keyword.</h5>`;
+    card(12, notFound);
+  } else if (typeof words[0] === "string") {
+    const youMean = `<h5 class="card-title text-center">Did you mean?</h5>`;
+    let btns = [];
+    for (let i = 0; i < words.length; i++) {
+      const btn = `<button type="button" class="suggest-btn btn btn-primary m-2">${words[i]} </button>`;
+      btns.push(btn);
+    }
+    btns = youMean + btns.join("");
+    card(12, btns);
+  } else {
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      const searchText = `<h5 class="card-title">${word.hwi.hw}</h5>
+        <p class="card-text">${word.fl}</p>
+        <p class="card-text">${word.shortdef}</p>`;
+      card(6, searchText);
+    }
+  }
+  const suggest__btn = document.querySelectorAll(".suggest-btn");
+  suggestionSearch(suggest__btn);
+}
+
+// function to suggest words results
+function suggestionSearch(btns) {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const value = e.target.innerText;
+      apiCall(value);
+    });
+  });
+}
+
+//function to display interesting words
+function interestingWords(words) {
+  for (let i = 0; i < 3; i++) {
+    const word = words[randomNum()];
+    const listItem = bootstrap("li", "list-group-item");
+    listItem.innerHTML = `<span><i class="far fa-arrow-right mr-2"></i></span>
+    <span>${word.name}</span>
+    <small class="d-block pl-4">${word.def}</small>`;
+    listGroup.appendChild(listItem);
+  }
+  cardRow.innerHTML = "";
+  const word = words[randomNum()];
+  const cardCol = bootstrap("div", "col-lg-12");
+  const card = bootstrap("div", "card mt-4");
+  const cardBody = bootstrap("div", "card-body overflow-auto");
+  cardBody.style.height = "15rem";
+  cardBody.innerHTML = `<h5 class="card-title">${word.name}</h5>
+      <p class="card-text">${word.def}</p>
+      <div class="img-contain">
+        <img src="/learn.jpeg" class="word-img w-100 h-100" alt="word-image" />
+      </div>
+      `;
+  card.appendChild(cardBody);
+  cardCol.appendChild(card);
+  cardRow.appendChild(cardCol);
+}
+interestingWords(iWords);
