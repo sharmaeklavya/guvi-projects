eaf774375d8a833cffbcce5219281c92c04ba0a2
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-05-06 16:31:04 +0530

    use effect

---
+++ sbadmin0405/react/src/products/list.js
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../usercontext";
+
+export default function ListProducts() {
+  const products = useContext(UserContext);
+  const [productList, setProductList] = useState([]);
+
+  // use effect
+  useEffect(async () => {
+    const getPromise = await fetch(
+      "https://6093bd12a7e53a0017951767.mockapi.io/products"
+    );
+    const getData = await getPromise.json();
+    setProductList([...getData]);
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid">
+        <h1 className="h3 mb-2 text-gray-800">Product Listing</h1>
+        <p className="mb-4">
+          <Link to="/createproduct">Create a new product</Link>
+        </p>
+
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">Products</h6>
+          </div>
+          <div className="card-body">
+            <div className="table-responsive">
+              <table
+                className="table table-bordered"
+                id="dataTable"
+                width="100%"
+                cellspacing="0"
+              >
+                <thead>
+                  <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Shop At</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {productList.map((product) => (
+                    <tr>
+                      <td> {product.name}</td>
+                      <td> {product.price}</td>
+                      <td> {product.quantity}</td>
+                      <td> {product.shop}</td>
+                      <td>
+                        <Link to={"/editproduct/" + product.id}>Edit</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ sbadmin0405/react/src/users/ListUser.js
@@ -0,0 +1,78 @@
+import { Link } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import UserContext from "../usercontext";
+
+export default function ListUsers(props) {
+  const users = useContext(UserContext);
+  const [userList, setUserList] = useState([]);
+  // use effect
+  useEffect(async () => {
+    const getPromise = await fetch(
+      "https://6093bd12a7e53a0017951767.mockapi.io/users"
+    );
+    const getData = await getPromise.json();
+    setUserList([...getData]);
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid">
+        <h1 className="h3 mb-2 text-gray-800">Registered Users</h1>
+        <p className="mb-4">
+          <Link to="/createuser">Create a new user</Link>
+        </p>
+
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">User List</h6>
+          </div>
+          <div className="card-body">
+            <div className="table-responsive">
+              <table
+                className="table table-bordered"
+                id="dataTable"
+                width="100%"
+              >
+                {userList.length > 0 ? (
+                  <>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {userList.map((user) => (
+                        <tr key={user.id}>
+                          <td> {user.name}</td>
+                          <td> {user.position}</td>
+                          <td> {user.location}</td>
+                          <td> {user.age}</td>
+                          <td> {user.startDate}</td>
+                          <td> {user.salary}</td>
+                          <td>
+                            <Link to={"/updateuser/" + user.id}>Update</Link>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <h3 className="h6">Loading...</h3>
+                  </>
+                )}
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
