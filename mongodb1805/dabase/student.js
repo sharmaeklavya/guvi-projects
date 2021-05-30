ccc696627f22789fde3d91dc59d2fc87bf81c56f
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-05-30 23:27:47 +0530

    first nodejs

---
+++ mongodb1805/dabase/student.js
@@ -0,0 +1,377 @@
+[
+  ({
+    mentor_id: "",
+    full_name: "Liana Wadeson",
+    email: "lwadeson0@hp.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Rozalie Lindsey",
+    email: "rlindsey1@geocities.jp",
+  },
+  {
+    mentor_id: "",
+    full_name: "Marla Housbie",
+    email: "mhousbie2@linkedin.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Desirae Greenham",
+    email: "dgreenham3@engadget.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Phil Grossier",
+    email: "pgrossier4@goo.gl",
+  },
+  {
+    mentor_id: "",
+    full_name: "Gabi Cushelly",
+    email: "gcushelly5@t.co",
+  },
+  {
+    mentor_id: "",
+    full_name: "Fran Bruniges",
+    email: "fbruniges6@smh.com.au",
+  },
+  {
+    mentor_id: "",
+    full_name: "Eduino Ollerenshaw",
+    email: "eollerenshaw7@hao123.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Lorenza Cribbins",
+    email: "lcribbins8@fastcompany.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Melamie Ketton",
+    email: "mketton9@sfgate.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Sonny Johnikin",
+    email: "sjohnikina@sina.com.cn",
+  },
+  {
+    mentor_id: "",
+    full_name: "Savina Killik",
+    email: "skillikb@webnode.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Adria McLagain",
+    email: "amclagainc@walmart.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Kacy Dance",
+    email: "kdanced@examiner.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Newton Dillon",
+    email: "ndillone@desdev.cn",
+  },
+  {
+    mentor_id: "",
+    full_name: "Leon Yersin",
+    email: "lyersinf@csmonitor.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Peterus Hobben",
+    email: "phobbeng@answers.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Orbadiah Inchley",
+    email: "oinchleyh@zdnet.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Norbie Tredget",
+    email: "ntredgeti@unesco.org",
+  },
+  {
+    mentor_id: "",
+    full_name: "Kurt Mattussevich",
+    email: "kmattussevichj@fc2.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Corty Winfindine",
+    email: "cwinfindinek@issuu.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Karen Maccaig",
+    email: "kmaccaigl@reverbnation.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Dedie Midson",
+    email: "dmidsonm@ucsd.edu",
+  },
+  {
+    mentor_id: "",
+    full_name: "Rolland Searson",
+    email: "rsearsonn@eepurl.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Miquela Titta",
+    email: "mtittao@google.cn",
+  },
+  {
+    mentor_id: "",
+    full_name: "Ronny Kuhnert",
+    email: "rkuhnertp@techcrunch.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Aubrey Philpot",
+    email: "aphilpotq@cpanel.net",
+  },
+  {
+    mentor_id: "",
+    full_name: "Fidela Talby",
+    email: "ftalbyr@uiuc.edu",
+  },
+  {
+    mentor_id: "",
+    full_name: "Clo Kinge",
+    email: "ckinges@scientificamerican.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Shepherd Dunlop",
+    email: "sdunlopt@discuz.net",
+  },
+  {
+    mentor_id: "",
+    full_name: "Mae Forstall",
+    email: "mforstallu@twitter.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Weston Gothliff",
+    email: "wgothliffv@jugem.jp",
+  },
+  {
+    mentor_id: "",
+    full_name: "Susie Kemery",
+    email: "skemeryw@columbia.edu",
+  },
+  {
+    mentor_id: "",
+    full_name: "Abigail Jobe",
+    email: "ajobex@vistaprint.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Teodora Coggin",
+    email: "tcogginy@skyrock.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Dolores Houselee",
+    email: "dhouseleez@nps.gov",
+  },
+  {
+    mentor_id: "",
+    full_name: "Rosanne Ruthven",
+    email: "rruthven10@tiny.cc",
+  },
+  {
+    mentor_id: "",
+    full_name: "Nerty Piggot",
+    email: "npiggot11@discovery.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Cob Durand",
+    email: "cdurand12@g.co",
+  },
+  {
+    mentor_id: "",
+    full_name: "Bo Urpeth",
+    email: "burpeth13@simplemachines.org",
+  },
+  {
+    mentor_id: "",
+    full_name: "Zak Mash",
+    email: "zmash14@bing.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Ron Stoven",
+    email: "rstoven15@spotify.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Leslie Enderlein",
+    email: "lenderlein16@elegantthemes.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Blithe Cluckie",
+    email: "bcluckie17@hhs.gov",
+  },
+  {
+    mentor_id: "",
+    full_name: "Katusha Palin",
+    email: "kpalin18@walmart.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Brenda Bromont",
+    email: "bbromont19@engadget.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Colline Pemble",
+    email: "cpemble1a@lycos.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Lila Korejs",
+    email: "lkorejs1b@tinyurl.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Junina Ainsbury",
+    email: "jainsbury1c@ask.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Maribel Cuningham",
+    email: "mcuningham1d@comsenz.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Greta Batty",
+    email: "gbatty1e@dailymotion.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Gwynne Nelius",
+    email: "gnelius1f@technorati.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Vi Ranns",
+    email: "vranns1g@ihg.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Harlan Silbert",
+    email: "hsilbert1h@ezinearticles.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Charline Dunstan",
+    email: "cdunstan1i@is.gd",
+  },
+  {
+    mentor_id: "",
+    full_name: "Shaylah Bartles",
+    email: "sbartles1j@time.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Wakefield Coote",
+    email: "wcoote1k@redcross.org",
+  },
+  {
+    mentor_id: "",
+    full_name: "Tabby Matthews",
+    email: "tmatthews1l@t.co",
+  },
+  {
+    mentor_id: "",
+    full_name: "Addia Luther",
+    email: "aluther1m@nba.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Allan Goodlake",
+    email: "agoodlake1n@eventbrite.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Che Nicklin",
+    email: "cnicklin1o@spiegel.de",
+  },
+  {
+    mentor_id: "",
+    full_name: "Marleah Forsbey",
+    email: "mforsbey1p@ovh.net",
+  },
+  {
+    mentor_id: "",
+    full_name: "Nellie Bertome",
+    email: "nbertome1q@de.vu",
+  },
+  {
+    mentor_id: "",
+    full_name: "Simona Vacher",
+    email: "svacher1r@nbcnews.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Daryn Iczokvitz",
+    email: "diczokvitz1s@woothemes.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Pavia Yosselevitch",
+    email: "pyosselevitch1t@virginia.edu",
+  },
+  {
+    mentor_id: "",
+    full_name: "Etti Conkey",
+    email: "econkey1u@salon.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Jan Skpsey",
+    email: "jskpsey1v@flavors.me",
+  },
+  {
+    mentor_id: "",
+    full_name: "Ethel Martinolli",
+    email: "emartinolli1w@sciencedirect.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Mauricio Confort",
+    email: "mconfort1x@google.com.au",
+  },
+  {
+    mentor_id: "",
+    full_name: "Ingrid Swinburne",
+    email: "iswinburne1y@wikispaces.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Thomasa Antoszczyk",
+    email: "tantoszczyk1z@msn.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Lyle Sturr",
+    email: "lsturr20@buzzfeed.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Giulia Aries",
+    email: "garies21@dell.com",
+  },
+  {
+    mentor_id: "",
+    full_name: "Beltran Smale",
+    email: "bsmale22@opera.com",
+  }),
+];
